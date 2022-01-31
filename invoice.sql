53183b2d61650e06aa55441b2dae0781fb8fc631
William Cuthbertson <whc1315@gmail.com>
2022-01-31 15:38:40 -0700

    Invoice Done

---
+++ invoice.sql
@@ -0,0 +1,22 @@
+SELECT COUNT(*) FROM invoice WHERE billing_country ILIKE 'USA';
+
+SELECT total FROM invoice ORDER BY total DESC LIMIT 1;
+
+SELECT total FROM invoice ORDER BY total ASC LIMIT 1;
+
+SELECT * FROM invoice WHERE total > 5;
+
+SELECT COUNT(*) FROM invoice WHERE total < 5;
+
+SELECT COUNT(*) FROM invoice WHERE billing_state IN('CA', 'TX', 'AZ');
+
+SELECT AVG(total) FROM invoice;
+
+SELECT SUM(total) FROM invoice;
+
+UPDATE invoice SET total = 24 WHERE invoice_id = 5;
+
+DELETE invoice FROM invoice WHERE invoice_id = 1;
+
+
+
