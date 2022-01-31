5a2191ad64d5bae7a3cd980acea41896bee7e0b3
William Cuthbertson <whc1315@gmail.com>
2022-01-31 14:46:22 -0700

    Artist finished

---
+++ artist.sql
@@ -0,0 +1,12 @@
+INSERT INTO artist (name, artist_id)
+VALUES ('Nickelbach', 276),
+('Daughtree', 277),
+('Three Days Grass', 278);
+
+SELECT * FROM artist WHERE artist_id <= 10 ORDER BY name DESC;
+
+SELECT * FROM artist WHERE artist_id <= 70 AND artist_id >= 66 ORDER BY name ASC;
+
+SELECT * FROM artist WHERE name = 'Black%'
+
+SELECT
